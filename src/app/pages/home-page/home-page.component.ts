8011edeca695e5f3024a0f31252454c994760315
k-flogs <kevin.kafg@gmail.com>
2026-04-08 11:05:17 +0200

    Conflictos solucionados

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { PlayersComponent } from '@app/components/players/players.component';
-import { Player } from '@app/shared/models/player.model';
-import { DetailComponent } from '@app/components/detail/detail.component';
-import { CommonModule } from '@angular/common';
-import { PLAYERS_DATA } from '@app/shared/data/players-list';
-import { AddPlayerComponent } from '@app/components/add-player/add-player.component';
-
-@Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [CommonModule, PlayersComponent, DetailComponent, AddPlayerComponent],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
-})
-export class HomePageComponent {
-
-  public players: Player[] = PLAYERS_DATA;
-  public selectedPlayer: Player | null = null;
-  public isAdding: boolean = false;
-
-  public handlePlayerSelection(player: Player): void {
-    this.selectedPlayer = { ...player };
-    this.isAdding = false;
-  }
-
-  public onNewPlayerClick(): void {
-    this.selectedPlayer = null;
-    this.isAdding = true;
-  }
-}
-=======
 import { Component } from '@angular/core';
 import { PlayersComponent } from '@app/components/players/players.component';
 import { Player } from '@app/shared/models/player.model';
@@ -64,4 +31,3 @@ export class HomePageComponent {
 
 
 }
->>>>>>> origin/develop
